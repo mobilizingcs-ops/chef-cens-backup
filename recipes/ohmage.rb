13ca9acf0893b11c97f90b5a682cf47670b67fb3
Steve Nolen <technolengy@gmail.com>
2015-04-02 06:59:14 -0700

    use chef-vault to grab ohmage db pw

--- recipes/ohmage.rb
+++ recipes/ohmage.rb
@@ -4,6 +4,12 @@
 #
 include_recipe 'cens-backup::default'
 
+# require chef-vault
+chef_gem 'chef-vault'
+require 'chef-vault'
+ohmage_db_password = ChefVault::Item.load('passwords', 'ohmage_db')
+fqdn = node['fqdn']
+
 backup_model :ohmage do
   description 'Back up ohmage database and data dirs'
 
@@ -12,7 +18,7 @@ backup_model :ohmage do
     database MySQL do |db|
       db.name = 'ohmage'
       db.username = 'ohmage'
-      db.password = '#{node['ohmage']['db']['password']}'
+      db.password = '#{ohmage_db_password[fqdn]}'
       db.host = 'localhost'
       db.port = '3306'
       db.additional_options = ['--quick', '--single-transaction']
