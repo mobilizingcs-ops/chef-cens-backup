fad3d452e1983e864b0539371eb725190333b436
Steve Nolen <technolengy@gmail.com>
2015-04-08 13:42:53 -0700

    unhack 639e8d77883, cens-backup::default now catches freebsd nodes and does less. offsite_lausd recipe is decent model for taking a backup tank and sending it offsite

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,42 @@
 # Recipe:: default
 #
 
-# having some weird trouble with nokogiri, so i'm going to install some extra packages so we can build it
-%w(libxslt-dev libxml2-dev liblzma-dev zlib1g-dev).each do |pkg|
-  package pkg
+case node[:platform]
+when 'freebsd'
+  node.set['backup']['config_path'] = '/usr/local/etc/backup'
+  node.set['backup']['model_path']   = "#{node['backup']['config_path']}/models"
+  node.set['backup']['group'] = 'wheel'
+
+  # prep with build-essential type stuff, but on freebsd
+  # in particular, gcc is no longer included as of freebsd 10
+  package 'gcc48'
+  link '/usr/local/bin/gcc48' do
+    to '/usr/local/bin/gcc'
+  end
+  link '/usr/local/bin/g++48' do
+    to '/usr/local/bin/g++'
+  end
+  package 'rsync' # since we plan to use rsync to ship
+else
+  %w(libxslt-dev libxml2-dev liblzma-dev zlib1g-dev).each do |pkg|
+    package pkg
+  end
+  include_recipe 'build-essential'
+  directory '/mnt/backups' do
+    owner 'root'
+    group 'root'
+    mode '0750'
+    action :create
+  end
+  
+  mount '/mnt/backups' do
+    device "starbuck.ohmage.org:/export/backups/#{node['fqdn']}"
+    fstype 'nfs'
+    options '_netdev,defaults,acl'
+    action [:mount, :enable]
+  end
 end
+
 
 # require chef-vault
 chef_gem 'chef-vault'
@@ -15,7 +47,6 @@ slack = ChefVault::Item.load('slack', 'backup-gem')
 webhook_url = slack['webhook']
 
 node.set['backup']['version'] = '4.1.7'
-include_recipe 'build-essential'
 include_recipe 'backup'
 begin
   # Replace global config file from backup cookbook with our own.
@@ -26,18 +57,4 @@ begin
   	)
 rescue Chef::Exceptions::ResourceNotFound
   Chef::Log.warn "could not find template to override!"
-end
-
-directory '/mnt/backups' do
-  owner 'root'
-  group 'root'
-  mode '0750'
-  action :create
-end
-
-mount '/mnt/backups' do
-  device "starbuck.ohmage.org:/export/backups/#{node['fqdn']}"
-  fstype 'nfs'
-  options '_netdev,defaults,acl'
-  action [:mount, :enable]
 end
