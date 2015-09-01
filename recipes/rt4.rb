00b8adfc313fd359c6ad1614790caeaacb6fa412
Steve Nolen <technolengy@gmail.com>
2015-09-01 07:14:02 -0700

    add s3 backup to rt4

--- recipes/rt4.rb
+++ recipes/rt4.rb
@@ -5,6 +5,12 @@
 # assumes rt4 installed from debian packages
 include_recipe 'cens-backup::default'
 
+# require chef-vault
+chef_gem 'chef-vault'
+require 'chef-vault'
+fqdn = node['fqdn']
+aws_creds = ChefVault::Item.load('aws', 'backup')
+
 backup_model :rt4 do
   description 'Back up database, config files, and source for rt4'
 
@@ -32,6 +38,21 @@ backup_model :rt4 do
     end
 
     compress_with Gzip
+
+    encrypt_with OpenSSL do |encryption|
+      encryption.password = '#{aws_creds["encryption_key"]}'
+      encryption.base64   = false
+      encryption.salt     = true
+    end
+
+    store_with S3 do |s3|
+      s3.access_key_id = '#{aws_creds["key"]}'
+      s3.secret_access_key = '#{aws_creds["secret"]}'
+      s3.bucket = 'mobilize-ohmage-backup'
+      s3.path = '#{node["fqdn"]}'
+      s3.chunk_size = 50
+      s3.keep = 1
+    end
 
     store_with Local do |local|
       local.path = '/mnt/backups/'
