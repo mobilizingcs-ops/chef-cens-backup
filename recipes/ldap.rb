16dddf769b27848b8d829014405e54b274e034be
Steve Nolen <technolengy@gmail.com>
2015-09-01 07:12:05 -0700

    add s3 backup to ldap model

--- recipes/ldap.rb
+++ recipes/ldap.rb
@@ -4,12 +4,34 @@
 #
 include_recipe 'cens-backup::default'
 
+# require chef-vault
+chef_gem 'chef-vault'
+require 'chef-vault'
+ohmage_db_password = ChefVault::Item.load('passwords', 'ohmage_db')
+fqdn = node['fqdn']
+aws_creds = ChefVault::Item.load('aws', 'backup')
+
 backup_model :ldap do
   description 'Back up openldap db'
 
   definition <<-DEF
 
     database OpenLDAP do |db|
+    end
+
+    encrypt_with OpenSSL do |encryption|
+      encryption.password = '#{aws_creds["encryption_key"]}'
+      encryption.base64   = false
+      encryption.salt     = true
+    end
+
+    store_with S3 do |s3|
+      s3.access_key_id = '#{aws_creds["key"]}'
+      s3.secret_access_key = '#{aws_creds["secret"]}'
+      s3.bucket = 'mobilize-ohmage-backup'
+      s3.path = '#{node["fqdn"]}'
+      s3.chunk_size = 50
+      s3.keep = 1
     end
 
     store_with Local do |local|
