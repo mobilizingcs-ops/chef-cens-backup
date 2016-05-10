dc05895383f32da67e0d1c9d09f67844e2a25ced
Steve Nolen <technolengy@gmail.com>
2016-05-10 13:04:04 -0700

    export owncloud set to docker host

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-backup'
 long_description 'Installs/Configures cens-backup'
-version '0.4.25'
+version '0.4.26'
 
 depends 'zfs'
 depends 'backup'
